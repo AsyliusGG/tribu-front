4d11cab0a6947a9d90fe6ba65386949bb2c75cd6
MrHutsa <vl.chamorro@duocuc.cl>
2024-09-17 12:11:27 -0300

    CTRL C +CTRL V

--- src/Components/RecentPost/RecentPosts.jsx
+++ src/Components/RecentPost/RecentPosts.jsx
@@ -1,36 +1,28 @@
+import { getallEventos } from '../api/api.js';
+import { useEffect, useState } from 'react';
+
 const RecentPosts = () => {
-  const posts = [
-    {
-      id: 1,
-      image: 'https://via.placeholder.com/150',
-      description: 'Lorem ipsum dolor sit amet.',
-    },
-    {
-      id: 2,
-      image: 'https://via.placeholder.com/150',
-      description: 'Qui consequuntur expedita.',
-    },
-    {
-      id: 3,
-      image: 'https://via.placeholder.com/150',
-      description: 'At recusandae expedita.',
-    },
-    {
-      id: 4,
-      image: 'https://via.placeholder.com/150',
-      description: 'Officia omnis qui.',
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadEventos() {
+      const response = await getallEventos();
+      console.log(response.data);
+      setPosts(response.data);
+    }
+
+    loadEventos();
+  }, []); 
 
   return (
     <div className="my-8">
       <h2 className="text-2xl font-bold text-center mb-6">POST RECIENTES</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4">
         {posts.map((post) => (
           <div key={post.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-            <img src={post.image} alt="Post" className="w-full h-48 object-cover" />
+            <img src={post.foto} alt="Post" className="w-full h-48 object-cover" />
             <div className="p-4">
-              <p>{post.description}</p>
+              <p>{post.descripcion}</p>
             </div>
           </div>
         ))}

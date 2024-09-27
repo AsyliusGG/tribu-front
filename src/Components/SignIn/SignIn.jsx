ca23a8cc70c5b2294e90ef14ef55fc34e42cdb55
MrHutsa <vl.chamorro@duocuc.cl>
2024-09-27 14:47:10 -0300

    alert
    cambio de alert utilizando el de tailwind

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,9 +1,38 @@
 import React, { useState } from 'react';
+import { Alert } from "@material-tailwind/react";
 
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function AlertCustomStyles({ message }) {
+  return (
+    <Alert
+      icon={<Icon />}
+      className="rounded-none border-l-4 border-[#2ec946] bg-[#2ec946]/10 font-medium text-[#2ec946] text-sm"
+    >
+      {message}
+    </Alert>
+  );
+}
 
 const SignIn = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,12 +41,12 @@ const SignIn = ({ onLogin }) => {
     const alphanumericRegex = /^[a-zA-Z0-9]{8,}$/;
 
     if (!alphanumericRegex.test(username)) {
-      alert('El nombre de usuario debe ser alfanumérico y tener al menos 8 caracteres.');
+      setAlertMessage('El nombre de usuario debe ser alfanumérico y tener al menos 8 caracteres.');
       return;
     }
 
     if (!alphanumericRegex.test(password)) {
-      alert('La contraseña debe ser alfanumérica y tener al menos 8 caracteres.');
+      setAlertMessage('La contraseña debe ser alfanumérica y tener al menos 8 caracteres.');
       return;
     }
 
@@ -25,7 +54,7 @@ const SignIn = ({ onLogin }) => {
     if (username === 'admin' && password === 'password') {
       onLogin(true);
     } else {
-      alert('Nombre de usuario o contraseña incorrectos');
+      setAlertMessage('Nombre de usuario o contraseña incorrectos');
     }
   };
 
@@ -33,6 +62,7 @@ const SignIn = ({ onLogin }) => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+        {alertMessage && <AlertCustomStyles message={alertMessage} />}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">

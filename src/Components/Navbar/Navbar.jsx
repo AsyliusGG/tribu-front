e92bbf1e6f002e01ff8ef829e1cf3cec593d7911
Nelly Becerra <nelly.becerrag@gmail.com>
2024-10-22 22:42:56 -0300

    cambios

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,11 +5,12 @@ import {
   MobileNav,
   Typography,
   Button,
-  IconButton
+  IconButton,
 } from "@material-tailwind/react";
 
-export default function StickyNavbar({ isAuthenticated }) {  // Usamos la prop isAuthenticated que viene de App.jsx
+export default function StickyNavbar({ isAuthenticated }) {
   const [openNav, setOpenNav] = useState(false);
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
 
   useEffect(() => {
     window.addEventListener(
@@ -18,39 +19,66 @@ export default function StickyNavbar({ isAuthenticated }) {  // Usamos la prop i
     );
   }, []);
 
-  // Función para cerrar sesión
   const handleLogout = () => {
-    localStorage.removeItem("auth_token");  // Elimina el token
-    window.location.href = "/";  // Redirige al inicio
+    localStorage.removeItem("auth_token");
+    window.location.href = "/";
+  };
+
+  const toggleProfileMenu = () => {
+    setOpenProfileMenu(!openProfileMenu);
   };
 
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
         <Link to="/Tribu" className="flex items-center">
           La Tribu
         </Link>
       </Typography>
-      <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
         <Link to="/Nosotras" className="flex items-center">
           Nosotras
         </Link>
       </Typography>
-      
-      {/* Solo mostrar estas opciones si el usuario está autenticado */}
+
       {isAuthenticated && (
         <>
-          <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
+          >
             <Link to="/Historia" className="flex items-center">
               Nuestra Historia
             </Link>
           </Typography>
-          <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
+          >
             <Link to="/Eventos" className="flex items-center">
               Eventos
             </Link>
           </Typography>
-          <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
+          <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-normal"
+          >
             <Link to="/Alianzas" className="flex items-center">
               Alianzas
             </Link>
@@ -64,20 +92,33 @@ export default function StickyNavbar({ isAuthenticated }) {  // Usamos la prop i
     <div className="sticky top-0 w-screen shadow-md max-h-[80vh] z-20">
       <Navbar className="z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography as="a" href="/" className="mr-4 cursor-pointer py-1.5 font-medium">
+          <Typography
+            as="a"
+            href="/"
+            className="mr-4 cursor-pointer py-1.5 font-medium"
+          >
             Tribu de Mamás
           </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
             <div className="flex items-center gap-x-1">
-              {/* Botón de carrito de compra */}
               {isAuthenticated && (
                 <Button variant="text" size="sm" className="flex items-center">
                   <Link to="/CarritoCompra" className="flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-</svg>
-
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      />
+                    </svg>
                     Carrito
                   </Link>
                 </Button>
@@ -85,21 +126,84 @@ export default function StickyNavbar({ isAuthenticated }) {  // Usamos la prop i
 
               {!isAuthenticated ? (
                 <>
-                  <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+                  <Button
+                    variant="gradient"
+                    size="sm"
+                    className="hidden lg:inline-block"
+                  >
                     <Link to="/login">
                       <span>Iniciar Sesión</span>
                     </Link>
                   </Button>
-                  <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+                  <Button
+                    variant="gradient"
+                    size="sm"
+                    className="hidden lg:inline-block"
+                  >
                     <Link to="/joinus">
                       <span>Registrarse</span>
                     </Link>
                   </Button>
                 </>
               ) : (
-                <Button variant="gradient" size="sm" onClick={handleLogout}>
-                  <span>Cerrar Sesión</span>
-                </Button>
+                <div className="relative">
+                  <img
+                    onClick={toggleProfileMenu}
+                    alt="Profile"
+                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+                    className="h-10 w-10 cursor-pointer rounded-full object-cover"
+                  />
+                  {openProfileMenu && (
+                    <ul className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20">
+                      <Typography
+                        as="li"
+                        variant="small"
+                        color="blue-gray"
+                        className="p-3 hover:bg-gray-100 cursor-pointer flex items-center"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                          />
+                        </svg>
+                        <span className="ml-2">Mi Perfil</span>
+                      </Typography>
+                      <hr className="border-t my-1" />
+                      <Typography
+                        as="li"
+                        variant="small"
+                        color="blue-gray"
+                        className="p-3 hover:bg-gray-100 cursor-pointer flex items-center"
+                        onClick={handleLogout}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="h-5 w-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                        <span className="ml-2">Cerrar Sesión</span>
+                      </Typography>
+                    </ul>
+                  )}
+                </div>
               )}
             </div>
             <IconButton
@@ -109,40 +213,39 @@ export default function StickyNavbar({ isAuthenticated }) {  // Usamos la prop i
               onClick={() => setOpenNav(!openNav)}
             >
               {openNav ? (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
                 </svg>
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth={2}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
                 </svg>
               )}
             </IconButton>
           </div>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <div className="flex items-center gap-x-1">
-            {!isAuthenticated ? (
-              <>
-                <Button fullWidth variant="gradient" size="sm">
-                  <Link to="/login">
-                    <span>Iniciar Sesión</span>
-                  </Link>
-                </Button>
-                <Button fullWidth variant="gradient" size="sm">
-                  <Link to="/joinus">
-                    <span>Registrarse</span>
-                  </Link>
-                </Button>
-              </>
-            ) : (
-              <Button fullWidth variant="gradient" size="sm" onClick={handleLogout}>
-                <span>Cerrar Sesión</span>
-              </Button>
-            )}
-          </div>
-        </MobileNav>
+        <MobileNav open={openNav}>{navList}</MobileNav>
       </Navbar>
     </div>
   );

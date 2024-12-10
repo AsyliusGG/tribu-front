cedaf45ac7663f54dc511ef4c7b5caf7a1489c20
AsyliusGG <fel.caceres@duocuc.cl>
2024-12-09 22:47:45 -0300

    Update EventosAdmin.jsx

--- src/Components/Eventos/EventosAdmin.jsx
+++ src/Components/Eventos/EventosAdmin.jsx
@@ -1,126 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {
-  Button,
-  Typography,
-  Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-  Alert,
-} from "@material-tailwind/react";
+import { Button, Typography, Card, CardHeader, CardBody, CardFooter } from "@material-tailwind/react";
+import axios from 'axios';
 import Cookies from "js-cookie";
 
-const EVENTOS_API_URL = "http://20.51.120.81:8000/api/v1/evento";
-const SECTOR_API_URL = "http://20.51.120.81:8000/api/v1/sector";
-
-const EventosAdmin = () => {
-  const [events, setEvents] = useState([]);
-  const [sectors, setSectors] = useState([]);
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [eventToDelete, setEventToDelete] = useState(null);
-  const [alertMessage, setAlertMessage] = useState("");
+const Eventos = () => {
   const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user); // Obtener el usuario desde el estado de Redux
+  const [posts, setPosts] = useState([]);
+  const [sectores, setSectores] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  // Cargar eventos y sectores desde la API
   useEffect(() => {
-    async function fetchData() {
+    const fetchEventos = async () => {
       try {
-        const [eventsResponse, sectorsResponse] = await Promise.all([
-          fetch(EVENTOS_API_URL),
-          fetch(SECTOR_API_URL),
-        ]);
-
-        if (eventsResponse.ok && sectorsResponse.ok) {
-          let eventsData = await eventsResponse.json();
-          const sectorsData = await sectorsResponse.json();
-
-          // Verificar si el evento fue ayer y desactivarlo si es necesario
-          const currentDate = new Date();
-          currentDate.setDate(currentDate.getDate() - 1); // Restar un día a la fecha actual
-          eventsData.forEach(async (event) => {
-            const eventDate = new Date(event.fecha);
-            if (eventDate < currentDate && !event.disabled) {
-              await desactivarEvento(event.id);
-              event.disabled = true; // Marcar como desactivado
-            }
-          });
-
-          setEvents(eventsData);
-          setSectors(sectorsData);
-        } else {
-          console.error("Error al obtener los eventos o sectores.");
-        }
+        const token = Cookies.get("auth_token");
+        const headers = {
+          'Authorization': `Bearer ${token}`
+        };
+        const eventosResponse = await axios.get("http://20.51.120.81:8000/api/v1/evento/", { headers });
+        const sectoresResponse = await axios.get("http://20.51.120.81:8000/api/v1/sector/", { headers });
+        const eventosActivos = eventosResponse.data.filter(evento => !evento.disabled);
+        setPosts(eventosActivos);
+        setSectores(sectoresResponse.data);
+        setLoading(false);
       } catch (error) {
-        console.error("Error al obtener los eventos o sectores:", error);
+        setError(error);
+        setLoading(false);
       }
-    }
+    };
 
-    fetchData();
+    fetchEventos();
   }, []);
-
-  const handleDelete = async (eventId) => {
-    try {
-      const token = Cookies.get("auth_token");
-      const response = await fetch(`${EVENTOS_API_URL}/${eventId}/`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        setEvents(events.filter((event) => event.id !== eventId));
-        setAlertMessage("Evento eliminado con éxito");
-      } else {
-        console.error("Error al eliminar el evento");
-        setAlertMessage("Error al eliminar el evento");
-      }
-    } catch (error) {
-      console.error("Error al eliminar el evento:", error);
-      setAlertMessage("Error al eliminar el evento");
-    }
-  };
-
-  const handleActivateDeactivate = async (eventId, isActive) => {
-    try {
-      const token = Cookies.get("auth_token");
-      const response = await fetch(`${EVENTOS_API_URL}/${eventId}/`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ disabled: !isActive }),
-      });
-
-      if (response.ok) {
-        setEvents(events.map((event) => (event.id === eventId ? { ...event, disabled: !isActive } : event)));
-        setAlertMessage(`Evento ${isActive ? "desactivado" : "activado"} con éxito`);
-      } else {
-        console.error(`Error al ${isActive ? "desactivar" : "activar"} el evento`);
-        setAlertMessage(`Error al ${isActive ? "desactivar" : "activar"} el evento`);
-      }
-    } catch (error) {
-      console.error(`Error al ${isActive ? "desactivar" : "activar"} el evento:`, error);
-      setAlertMessage(`Error al ${isActive ? "desactivar" : "activar"} el evento`);
-    }
-  };
-
-  const handleModify = (eventId) => {
-    navigate(`/Eventos/ModificarEvento/${eventId}`);
-  };
-
-  const openDeleteDialog = (eventId) => {
-    setEventToDelete(eventId);
-    setDeleteDialogOpen(true);
-  };
-
-  const closeDeleteDialog = () => {
-    setDeleteDialogOpen(false);
-    setEventToDelete(null);
-  };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -129,6 +42,15 @@ const EventosAdmin = () => {
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
+  const openDeleteDialog = (event) => {
+    setEventToDelete(event);
+    setDeleteDialogOpen(true);
+  };
+
+  const closeDeleteDialog = () => {
+    setEventToDelete(null);
+    setDeleteDialogOpen(false);
+  };
 
   const formatTime = (timeString) => {
     const [hour, minute] = timeString.split(':');
@@ -136,100 +58,64 @@ const EventosAdmin = () => {
   };
 
   const getSectorName = (sectorId) => {
-    const sector = sectors.find((sector) => sector.id === sectorId);
+    const sector = sectores.find((sector) => sector.id === sectorId);
     return sector ? sector.sector_nombre : "Desconocido";
   };
 
-  return (
-    <div className="container mx-auto px-4">
-      <Typography variant="h2" color="blue-gray" className="text-center mb-10">
-        Administrador de Eventos
-      </Typography>
-      {alertMessage && (
-        <Alert color="red" className="mb-4">
-          {alertMessage}
-        </Alert>
-      )}
-      <table className="min-w-full bg-white">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b">Nombre</th>
-            <th className="py-2 px-4 border-b">Fecha</th>
-            <th className="py-2 px-4 border-b">Hora</th>
-            <th className="py-2 px-4 border-b">Sector</th>
-            <th className="py-2 px-4 border-b">Cupos Totales</th>
-            <th className="py-2 px-4 border-b">Cupos Disponibles</th>
-            <th className="py-2 px-4 border-b">Estado</th>
-            <th className="py-2 px-4 border-b">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {events.map((event) => (
-            <tr key={event.id}>
-              <td className="py-2 px-4 border-b">{event.nombre}</td>
-              <td className="py-2 px-4 border-b">{formatDate(event.fecha)}</td>
-              <td className="py-2 px-4 border-b">{formatTime(event.hora)}</td>
-              <td className="py-2 px-4 border-b">{getSectorName(event.sector)}</td>
-              <td className="py-2 px-4 border-b">{event.cupos_totales}</td>
-              <td className="py-2 px-4 border-b">{event.cupos_disponibles}</td>
-              <td className="py-2 px-4 border-b">{event.disabled ? "Desactivado" : "Activo"}</td>
-              <td className="py-2 px-4 border-b">
-                <div className="flex space-x-2">
-                  <Button
-                    variant="gradient"
-                    color="green"
-                    onClick={() => handleActivateDeactivate(event.id, !event.disabled)}
-                  >
-                    {event.disabled ? "Activar" : "Desactivar"}
-                  </Button>
-                  <Button
-                    variant="gradient"
-                    color="blue"
-                    onClick={() => handleModify(event.id)}
-                  >
-                    Modificar
-                  </Button>
-                  <Button
-                    variant="gradient"
-                    color="red"
-                    onClick={() => openDeleteDialog(event.id)}
-                  >
-                    Eliminar
-                  </Button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+  const goToAdmin = () => {
+    navigate("/Eventos/EventosAdmin");
+  };
 
-      {deleteDialogOpen && (
-        <Dialog open={deleteDialogOpen} handler={closeDeleteDialog}>
-          <DialogHeader>Confirmar eliminación</DialogHeader>
-          <DialogBody>¿Desea realmente eliminar este evento?</DialogBody>
-          <DialogFooter>
-            <Button
-              variant="text"
-              color="blue-gray"
-              onClick={closeDeleteDialog}
-            >
-              No
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="bg-gray-100 py-10">
+      <div className="container mx-auto px-4">
+        {/* Botón de Administrador de Eventos */}
+        {user && user.is_staff && (
+          <div className="flex justify-end mb-6">
+            <Button variant="gradient" color="blue" onClick={goToAdmin}>
+              Administrador de Eventos
             </Button>
-            <Button
-              variant="gradient"
-              color="red"
-              onClick={() => {
-                handleDelete(eventToDelete);
-                closeDeleteDialog();
-              }}
-            >
-              Sí
-            </Button>
-          </DialogFooter>
-        </Dialog>
-      )}
+          </div>
+        )}
+        
+        <Typography variant="h2" color="blue-gray" className="text-center mb-10">
+          Nuestros Eventos
+        </Typography>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {posts.map((post) => (
+            <Card key={post.id} className="w-full h-full flex flex-col justify-between cursor-pointer">
+              <CardHeader color="blue-gray" className="relative h-56">
+                <img src={post.foto} alt={post.descripcion} className="w-full h-full object-cover" />
+              </CardHeader>
+              <CardBody>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {post.nombre}
+                </Typography>
+                <Typography color="blue-gray" className="mb-2">
+                  {formatDate(post.fecha)} - {formatTime(post.hora)}
+                </Typography>
+                <Typography color="blue-gray" className="mb-2">
+                  Sector: {getSectorName(post.sector)}
+                </Typography>
+                <Typography color="blue-gray" className="mb-2">
+                  Lugar: {post.lugar}
+                </Typography>
+              </CardBody>
+              <CardFooter className="flex justify-end">
+                <Button variant="gradient" color="blue" onClick={() => navigate(`/Eventos/VerEvento/${post.id}`)}>
+                  Ver Detalles
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default EventosAdmin;
+export default Eventos;

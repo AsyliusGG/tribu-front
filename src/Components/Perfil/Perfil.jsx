97e18ec98e3097d342037c47f6c5d1afbc84a2b4
AsyliusGG <fel.caceres@duocuc.cl>
2024-12-27 16:02:38 -0300

    Update Perfil.jsx

--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -24,6 +24,10 @@ const Perfil = () => {
     sector: user?.sector || '',
     know_source: user?.know_source || '',
   });
+  const [sectors, setSectors] = useState([]);
+  const [knowSources, setKnowSources] = useState([]);
+  const [wspSector, setWspSector] = useState('');
+  const [updatedUser, setUpdatedUser] = useState(user);
 
   useEffect(() => {
     if (user) {
@@ -31,9 +35,36 @@ const Perfil = () => {
         first_name: user.first_name,
         last_name: user.last_name,
         email: user.email,
+        phone_number: user.phone_number,
+        run: user.run,
+        birthday: user.birthday,
+        job: user.job,
+        sector: user.sector,
+        know_source: user.know_source,
       });
     }
   }, [user]);
+
+  useEffect(() => {
+    // Obtener los datos de SECTOR
+    axios.get('http://20.51.120.81:8000/api/v1/sector/').then(response => {
+      setSectors(response.data);
+    });
+
+    // Obtener los datos de KNOW SOURCE
+    axios.get('http://20.51.120.81:8000/api/v1/knowsource/').then(response => {
+      setKnowSources(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (formData.sector) {
+      const sector = sectors.find(sector => sector.id === formData.sector);
+      if (sector) {
+        setWspSector(sector.wsp_sector);
+      }
+    }
+  }, [formData.sector, sectors]);
   
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -49,17 +80,18 @@ const Perfil = () => {
 
   const handleSave = async (e) => {
     e.preventDefault();
-    // Verificar que todos los campos requeridos estén presentes
+  
+    // Verificar campos requeridos
     if (!formData.first_name || !formData.last_name || !formData.email || 
         !formData.phone_number || !formData.run || !formData.birthday || 
         !formData.job || !formData.sector || !formData.know_source) {
-      console.error('Todos los campos son obligatorios');
       alert('Todos los campos son obligatorios');
       return;
     }
   
     try {
-      const response = await axios.put(
+      // Actualizar usuario en el backend
+      await axios.put(
         'http://20.51.120.81:8000/api/v1/auth/users/me/',
         {
           first_name: formData.first_name,
@@ -79,11 +111,46 @@ const Perfil = () => {
           },
         }
       );
-      // Actualiza el usuario en el estado de Redux si es necesario
-      console.log('Información actualizada con éxito:', response.data);
-      setEditing(false);
+  
+      // Obtener los datos actualizados del backend
+      const response = await axios.get('http://20.51.120.81:8000/api/v1/auth/users/me/', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      // Actualizar estado con los datos del usuario actualizado
+      setUpdatedUser(response.data);
+      setEditing(false); // Salir del modo edición
+      alert('Información actualizada con éxito');
     } catch (error) {
       console.error('Error al actualizar la información:', error.response || error.message);
+      if (error.response) {
+        alert(`Error: ${error.response.data.detail || 'Ocurrió un error al actualizar la información'}`);
+      } else {
+        alert('Ocurrió un error inesperado al actualizar la información');
+      }
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Aquí puedes agregar la lógica para enviar los datos actualizados al servidor
+      console.log('Datos del formulario actualizados:', formData);
+      // Actualizar los mapeos después de guardar los cambios
+      const sector = sectors.find(sector => sector.id === formData.sector);
+      const knowSource = knowSources.find(source => source.id === formData.know_source);
+      setWspSector(sector ? sector.wsp_sector : '');
+      setFormData({
+        ...formData,
+        sector: sector ? sector.sector_nombre : formData.sector,
+        know_source: knowSource ? knowSource.source_name : formData.know_source,
+      });
+
+      // Volver a leer el endpoint user/me
+      const response = await axios.get('http://20.51.120.81:8000/api/v1/auth/users/me');
+      setUpdatedUser(response.data);
+    } catch (error) {
       if (error.response) {
         // Mostrar mensaje de error específico del servidor
         alert(`Error: ${error.response.data.detail || 'Ocurrió un error al actualizar la información'}`);
@@ -129,9 +196,19 @@ const Perfil = () => {
     
   };
 
+  const getSectorName = (id) => {
+    const sector = sectors.find(sector => sector.id === id);
+    return sector ? sector.sector_nombre : '';
+  };
+
+  const getKnowSourceName = (id) => {
+    const source = knowSources.find(source => source.id === id);
+    return source ? source.source_name : '';
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-3xl bg-white p-6 rounded-lg shadow-md">
+      <div onSubmit={handleSubmit} className="w-full max-w-3xl bg-white p-6 rounded-lg shadow-md">
         <h1 className="text-2xl font-bold text-gray-800 text-center mb-4">Mi Perfil</h1>
         {user ? (
           <div>
@@ -209,27 +286,44 @@ const Perfil = () => {
                 </div>
                 <div className="mb-4">
                   <label className="block text-gray-700">Sector:</label>
-                  <input
-                    type="number"
+                  <select
+                    id="sector"
                     name="sector"
                     value={formData.sector}
                     onChange={handleInputChange}
                     className="w-full px-3 py-2 border rounded"
-                  />
+                  >
+                    {sectors.map((sector) => (
+                      <option key={sector.id} value={sector.id}>
+                        {sector.sector_nombre}
+                      </option>
+                    ))}
+                  </select>
                 </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700">Fuente de conocimiento:</label>
-                  <input
-                    type="number"
+                <div>
+                  <label className="block text-gray-700">¿Cómo nos conociste?</label>
+                  <select
+                    id="knowSource"
                     name="know_source"
                     value={formData.know_source}
                     onChange={handleInputChange}
                     className="w-full px-3 py-2 border rounded"
-                  />
+                  >
+                    {knowSources.map((source) => (
+                      <option key={source.id} value={source.id}>
+                        {source.source_name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
+                <br />
                 <button
                   className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200"
-                  onClick={handleSave}
+                  onClick={async (e) => {
+                    await handleSave(e);
+                    const response = await axios.get('http://20.51.120.81:8000/api/v1/auth/users/me');
+                    setUpdatedUser(response.data);
+                  }}
                 >
                   Guardar
                 </button>
@@ -238,35 +332,39 @@ const Perfil = () => {
               <div>
                 <p className="text-gray-700 text-lg">
                   <span className="font-semibold">Nombre: </span>
-                  {user.first_name} {user.last_name}
+                  {updatedUser.first_name} {updatedUser.last_name}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
                   <span className="font-semibold">Correo electrónico: </span>
-                  {user.email}
+                  {updatedUser.email}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
                   <span className="font-semibold">Número de teléfono: </span>
-                  {user.phone_number}
+                  {updatedUser.phone_number}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
                   <span className="font-semibold">RUN: </span>
-                  {user.run}
+                  {updatedUser.run}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
                   <span className="font-semibold">Fecha de nacimiento: </span>
-                  {user.birthday}
+                  {updatedUser.birthday}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
                   <span className="font-semibold">Trabajo: </span>
-                  {user.job}
+                  {updatedUser.job}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
-                  <span className="font-semibold">Sector: </span>
-                  {user.sector}
+                  <span onChange={handleInputChange} className="font-semibold">Sector: </span>
+                  {getSectorName(updatedUser.sector)}
                 </p>
                 <p className="text-gray-700 text-lg mt-2">
-                  <span className="font-semibold">Fuente de conocimiento: </span>
-                  {user.know_source}
+                  <span onChange={handleInputChange} className="font-semibold">¿Cómo nos conociste? </span>
+                  {getKnowSourceName(updatedUser.know_source)}
+                </p>
+                <p className="text-gray-700 text-lg mt-2">
+                  <span className="font-semibold">Grupo de WhatsApp: </span>
+                  {wspSector}
                 </p>
                 <button
                   className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200 mt-4"
